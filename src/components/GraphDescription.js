fa5414a472528622162657ec495fa2825b9c2c3e
Bhuwann <bhuwanbhattarai04@gmail.com>
2024-07-09 01:47:33 -0400

    Refactor and document GraphDescription.js

--- src/components/GraphDescription.js
+++ src/components/GraphDescription.js
@@ -1,63 +1,55 @@
 import React, { useState } from 'react';
 import '../App.css';
 import StockChart from '../components/StockChart.js';
-import {rawGraphDescription} from "../DefaultData.js";
-import {normalizedGraphDescription} from "../DefaultData.js";
-import {spreadGraphDescription} from "../DefaultData.js";
-import {equityGraphDescription} from "../DefaultData.js";
-import {backtest, calculateEuclideanDifference} from "./Calculations.js";
+import { rawGraphDescription, normalizedGraphDescription, spreadGraphDescription, equityGraphDescription } from "../DefaultData.js";
+import { backtest, calculateEuclideanDifference } from "./Calculations.js";
 import Latex from 'react-latex';
 
-
-
+/**
+ * GraphDescription component renders different types of stock charts with descriptions.
+ *
+ * @param {Object} props - Component props
+ * @param {Object} props.data1 - Data for the first stock
+ * @param {Object} props.data2 - Data for the second stock
+ * @param {string} props.chartType - Type of chart to display ('raw', 'normalized', 'spread', or 'equity')
+ * @returns {JSX.Element} Rendered component
+ */
 const GraphDescription = ({ data1, data2, chartType }) => {
-    const [isChecked, setIsChecked] = useState(true)
+    const [isChecked, setIsChecked] = useState(true);
+
     const euclideanDistance = (data1 && data2) ? calculateEuclideanDifference(data1, data2) : 0;
     const formulaSSD = `$$SSD = \\sum_{i=0}^{n} (p1_i - p2_i)^2 = ${euclideanDistance}$$`;
-    const tickerSubtractionFormula = `$$P_{${data1.symbol}} - P_{${data2.symbol}}$$`
+    const tickerSubtractionFormula = `$$P_{${data1?.symbol}} - P_{${data2?.symbol}}$$`;
     const backtestEquitySeries = chartType === 'equity' ? backtest(data1, data2) : null;
-    const backtestProfit = chartType === 'equity' ? backtestEquitySeries[backtestEquitySeries.length - 1].toFixed(2) : 69;
+    const backtestProfit = chartType === 'equity' ? backtestEquitySeries[backtestEquitySeries.length - 1].toFixed(2) : 0;
     const profitValue = `$$Profit = ${backtestProfit}$$`;
 
-    const handleClick = () => {
-        setIsChecked(!isChecked);
-    }
+    const handleClick = () => setIsChecked(!isChecked);
 
-    let display = isChecked ? 'Hide' : 'Show'
-    if (chartType === 'raw') {
-        display = isChecked ? 'Hide Price Series' : 'Show Price Series'
-    } else if (chartType === 'normalized') {
-        display = isChecked ? 'Hide Normalized Price Series' : 'Show Normalized Price Series'
-    } else if (chartType === 'spread') {
-        display = isChecked ? 'Hide Spread Price Series' : 'Show Spread Price Series'
-    } else if (chartType === 'equity') {
-        display = isChecked ? 'Hide Backtest Results' : 'Show Backtest Results'
-    }
+    const getDisplayText = () => {
+        const baseText = isChecked ? 'Hide' : 'Show';
+        switch (chartType) {
+            case 'raw': return `${baseText} Price Series`;
+            case 'normalized': return `${baseText} Normalized Price Series`;
+            case 'spread': return `${baseText} Spread Price Series`;
+            case 'equity': return `${baseText} Backtest Results`;
+            default: return baseText;
+        }
+    };
 
-    return (
-        <>
-            <div className='toggle-container'>
-                <label className="cursor-pointer label pr-3">
-                    <input onChange={handleClick} type="checkbox" className="toggle toggle-primary"
-                           checked={isChecked}/>
-                </label>
-                <div className='toggle-text'>
-                    {display}
-                </div>
-            </div>
-            {isChecked && (
-            <div className='description-graph-container'>
-                {chartType === 'raw' && (
+    const renderChartContent = () => {
+        switch (chartType) {
+            case 'raw':
+                return (
                     <>
-                        <div className='description-container'>
-                            {rawGraphDescription}
-                        </div>
+                        <div className='description-container'>{rawGraphDescription}</div>
                         <div className="graph">
                             <StockChart data1={data1} data2={data2} chartType='raw'/>
                         </div>
                     </>
-                )}
-                {chartType === 'normalized' && (
+                );
+            case 'normalized':
+                return (
                     <>
                         <div className='description-container'>
                             {normalizedGraphDescription}
@@ -69,8 +61,9 @@ const GraphDescription = ({ data1, data2, chartType }) => {
                             <StockChart data1={data1} data2={data2} chartType='normalized' risk={2}/>
                         </div>
                     </>
-                )}
-                {chartType === 'spread' && (
+                );
+            case 'spread':
+                return (
                     <>
                         <div className='description-container'>
                             {spreadGraphDescription}
@@ -78,14 +71,16 @@ const GraphDescription = ({ data1, data2, chartType }) => {
                                 <Latex>{tickerSubtractionFormula}</Latex>
                             </div>
                             <div>
-                                <span style={{ color: 'blue' }}>BLUE</span> and <span style={{ color: 'red' }}>RED</span> indicate two historical deviations of spread price                            </div>
+                                <span style={{ color: 'blue' }}>BLUE</span> and <span style={{ color: 'red' }}>RED</span> indicate two historical deviations of spread price
+                            </div>
                         </div>
                         <div className="graph">
                             <StockChart data1={data1} data2={data2} chartType='spread'/>
                         </div>
                     </>
-                )}
-                {chartType === 'equity' && (
+                );
+            case 'equity':
+                return (
                     <>
                         <div className='description-container'>
                             {equityGraphDescription}
@@ -98,12 +93,32 @@ const GraphDescription = ({ data1, data2, chartType }) => {
                             <StockChart data1={data1} data2={data2} chartType='equity' equitySeries={backtestEquitySeries}/>
                         </div>
                     </>
-                )}
+                );
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <>
+            <div className='toggle-container'>
+                <label className="cursor-pointer label pr-3">
+                    <input
+                        onChange={handleClick}
+                        type="checkbox"
+                        className="toggle toggle-primary"
+                        checked={isChecked}
+                    />
+                </label>
+                <div className='toggle-text'>{getDisplayText()}</div>
             </div>
+            {isChecked && (
+                <div className='description-graph-container'>
+                    {renderChartContent()}
+                </div>
             )}
         </>
-    )
-}
-
+    );
+};
 
 export default GraphDescription;
